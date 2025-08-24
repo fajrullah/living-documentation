3e4bfdcce472b7586f6db882e486fbb919204c43
Fajrullah <fajrullahsejati_9_7@yahoo.com>
2025-08-24 09:02:15 +0700

    fix: storybook js

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/storybook/:path*",
+        destination: "/storybook/:path*", // static files from public
+      },
+    ];
+  },
 };
 
 export default nextConfig;
