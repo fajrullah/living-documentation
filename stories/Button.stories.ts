7d0a5057a72ff268f876005acf1f138b2a9f4370
Fajrullah <fajrullahsejati_9_7@yahoo.com>
2025-08-25 17:14:29 +0700

    feat: add image or svg + tidy

--- stories/Button.stories.ts
+++ stories/Button.stories.ts
@@ -4,7 +4,7 @@ import { Button } from "./Button";
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
-  title: "Example/Button",
+  title: "Branding/Button",
   component: Button,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
@@ -41,6 +41,7 @@ export const Secondary: Story = {
 
 export const Large: Story = {
   args: {
+    primary: true,
     size: "large",
     label: "Button",
   },
@@ -48,6 +49,7 @@ export const Large: Story = {
 
 export const Small: Story = {
   args: {
+    primary: true,
     size: "small",
     label: "Button",
   },
